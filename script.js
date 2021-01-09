0f3d11053f10e6154f27c119596b92dacafcecf1
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2021-01-09 17:04:18 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let url = "https://script.google.com/macros/s/AKfycbyM5GedbvM6ygiGEpJ94E1Ahve_05F63-GN5Y6GUBtD_muSiKE/exec";
+let url = "https://script.google.com/macros/s/AKfycbyaBnlqEmtPHsJlOztldIgrbhd0VKTq17Ok9Q16ckJbdv8pvhk/exec";
 let output = document.getElementById("output");
 let input = document.getElementById("input");
 let submit = document.getElementById("submit"),
 	reload = document.getElementById("reload");
 
 submit.addEventListener("click",function(){
-	doGet()
+	doPost()
 });
 reload.addEventListener("click",function(){
 	window.location.reload()
@@ -13,19 +13,19 @@ reload.addEventListener("click",function(){
 
 function doPost(){
 	let requests = new XMLHttpRequest();
-	requests.open("POST",url,true);
-	requests.send("?name="+input.value)
+	requests.open("POST", url, true);
+	requests.send("name=Dung");
 	return 0;
 }
 
 function doGet(){
 	let requests = new XMLHttpRequest();
-	requests.open("GET",url+"?name="+input.value);
-	/*requests.onreadystatechange = function(){
+	requests.open("GET",url);
+	requests.onreadystatechange = function(){
 		if(this.readyState == 4){
 			output.innerHTML = this.responseText;
 		}
-	}*/
+	}
 	requests.send()
 	return 0;
 }
