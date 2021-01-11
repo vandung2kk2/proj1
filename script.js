e021534098c5af18ac70900d1e43467a309bcbdc
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2021-01-11 11:09:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function doPost(){
 	requests.onreadystatechange = function(){
 		if(this.readyState == 4) output.innerHTML = "Successfuly !";
 	}
-	requests.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	requests.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=utf-8");
 	requests.send(input.value);
 	return 0;
 }
